6669d5cc3b697fffa041eb2faa1502085fcf01bc
naxida-ima <naxida-ima@users.noreply.github.com>
2026-08-23 21:20:23 +0000

    fix: PendingIntent import

--- app/src/main/java/com/nahida/touchmap/overlay/OverlayService.kt
+++ app/src/main/java/com/nahida/touchmap/overlay/OverlayService.kt
@@ -3,6 +3,7 @@ package com.nahida.touchmap.overlay
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
 import android.content.Context
 import android.content.Intent
